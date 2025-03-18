e096ed8386b3604f17db02852c0d592fe23107f0
TsunaHalo2001 <joan.villamil@correounivalle.edu.co>
2025-03-18 12:40:19 -0500

    Finished both

--- app/src/main/scala/taller/Lista.scala
+++ app/src/main/scala/taller/Lista.scala
@@ -11,4 +11,15 @@ class Lista() {
     else if (l2.nonEmpty) ejercicio1(l1, l2.tail) :+ l2.head
     else Nil
   }
+
+  @annotation.tailrec
+  final def factorial(n: Int, acc: Int = 1): Int = {
+    if (n <= 0) acc
+    else factorial(n - 1, n * acc)
+  }
+
+  def ejercicio2(n: Int, i: Int = 1 ,acc:List[Int]=List(1)): List[Int] = {
+    if (n < i) acc
+    else ejercicio2(n, i + 1, acc :+ factorial(i))
+  }
 }
--- app/src/test/scala/taller/ListaTest.scala
+++ app/src/test/scala/taller/ListaTest.scala
@@ -8,7 +8,15 @@ import org.scalatestplus.junit.JUnitRunner
 class ListaTest extends AnyFunSuite {
   val objBuscarLista = new Lista()
 
-  test("Encontrar 3 en List(1, 2, 3, 4, 5)") {
+  test("ejercicio1") {
     assert(objBuscarLista.ejercicio1(List(1, 2, 3), List(4, 5, 6)) === List(6, 5, 4, 3, 2, 1))
+  }
+
+  test("factorial") {
+    assert(objBuscarLista.factorial(5) === 120)
+  }
+
+  test("ejercicio2") {
+    assert(objBuscarLista.ejercicio2(5) === List(1, 1, 2, 6, 24, 120))
   }
 }
